c3e594b0b80003156d3016840d2d230e41e11c98
Sayantan Mandal <sayantanmandal174@gmail.com>
2023-05-25 10:24:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 //complete this code
 class Person {
-	constructor(name,age){
-		this.name=name;
-		this.age=age;
-	}
-	get name(){
-		return name;
-	}
-	set age(age){
-		this.age=age;
-	}
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get age() {
+    return this._age;
+  }
+
+  set age(newAge) {
+    this._age = newAge;
+  }
 }
 
 class Student extends Person {
